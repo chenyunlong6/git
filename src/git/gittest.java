1ad6768624818cc0a4cc6014bc5bd5db2e43cdbf
cyl <a6657305@qq.com>
2018-06-23 14:38:58 +0800

    test

---
+++ src/git/gittest.java
@@ -0,0 +1,12 @@
+package git;
+
+public class gittest {
+	public void a(){
+		System.out.print("test");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new gittest().a();
+	}
+
+}
